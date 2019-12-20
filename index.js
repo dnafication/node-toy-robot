e6bad5d2f8203f25bf55bab2ca46f284160994c5
Dina <dnafication@users.noreply.github.com>
2019-12-20 17:36:54 +1100

    Added commander to parse options

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const program = require('commander');
 /**
  * This is the main app which creates the environment and also initializes the
  * cli to receive standard input
  */
+
+program
+  .version('1.0.0')
+  .description('An application to simulate a toy robot moving on a Table')
+  .option(
+    '-f, --file <inputFile>',
+    'optional file flag to use the robot with instructions from a file'
+  )
+  .parse(process.argv);
 
 const Robot = require('./lib/Robot');
 const Table = require('./lib/Table');
@@ -13,5 +23,4 @@ const table = new Table(5, 5);
 const robot = new Robot();
 
 // default params for place
-
 debugger;
