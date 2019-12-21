f77931859106c3405ca8baa0301b46cea247787d
dnafication <dnafication@users.noreply.github.com>
2019-12-21 14:11:55 +1100

    main application arranged

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const program = require('commander');
+const fs = require('fs');
+const readline = require('readline');
+
+const Robot = require('./lib/Robot');
+const Table = require('./lib/Table');
+const { parseAndAct } = require('./lib/utils');
+
 /**
  * This is the main app which creates the environment and also initializes the
  * cli to receive standard input
  */
+
+// create the table object. 5 by 5 as per the problem statement
+const table = new Table(5, 5);
+
+// create the robot and place it on the table
+const robot = new Robot();
 
 program
   .version('1.0.0')
@@ -13,14 +26,61 @@ program
   )
   .parse(process.argv);
 
-const Robot = require('./lib/Robot');
-const Table = require('./lib/Table');
+console.log('NODE TOY ROBOT 🤖');
+console.log('--------------');
 
-// create the table object. 5 by 5 as per the problem statement
-const table = new Table(5, 5);
+if (program.file) {
+  console.log(
+    `Operating the robot based on commands provided in file: ${program.file}`
+  );
 
-// create the robot and place it on the table
-const robot = new Robot();
+  fs.stat(program.file, (err, stats) => {
+    if (err) return console.error('Error while checking the file', err);
+    if (stats.isFile()) {
+      fs.readFile(program.file, 'utf8', (err, data) => {
+        if (err) return console.error('Error while opening the file', err);
+        const commands = data.split('\n');
+        commands.forEach(cmd => {
+          parseAndAct(cmd, table, robot);
+        });
+      });
+    }
+  });
+} else {
+  console.log(
+    `Welcome to Node Toy Robot. We have created a Table (of dimension 5x5)\n` +
+      `and a Robot. You can issue following commands to operate the\n` +
+      `Robot on the Table:\n` +
+      `  - PLACE X,Y,F\n` +
+      `  - MOVE\n` +
+      `  - LEFT\n` +
+      `  - RIGHT\n` +
+      `  - REPORT\n\n` +
+      `Start the program with option '--file <filename>' to use commands from file\n` +
+      `Learn more by running the program with '--help' flag\n` +
+      `For more info on constraints please read the file ./node-toy-robot.md\n` +
+      `Type 'q' or 'exit' to exit the program\n` +
+      `Unknown or invalid commands are ignored by the program\n`
+  );
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
+  });
+
+  rl.prompt();
+
+  rl.on('line', command => {
+    if (command.toLowerCase() === 'q' || command.toLowerCase() === 'exit') {
+      return rl.close();
+    }
+    parseAndAct(command, table, robot);
+    return rl.prompt();
+  }).on('close', () => {
+    console.log('Good bye!');
+    process.exit(0);
+  });
+}
 
 // default params for place
-debugger;
+// debugger;
